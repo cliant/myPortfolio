83b6f678201cfa8f7014c7714ea0a3793b8bdc49
jun <uamy2222@gmail.co>
2025-09-22 11:50:20 +0900

    style: 모바일 스타일링

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,27 @@
-// src/components/header.tsx
-import { Link } from 'react-router-dom'; // Link 컴포넌트 import
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className="logo">Jun's Portfolio</div>
-      <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/about">About me</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/interview">Interview</Link>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        <span className="hamburger-icon"></span>
+        <span className="hamburger-icon"></span>
+        <span className="hamburger-icon"></span>
+      </button>
+      <nav className={`nav ${isMenuOpen ? 'nav-open' : ''}`}>
+        <Link to="/" onClick={toggleMenu}>Home</Link>
+        <Link to="/about" onClick={toggleMenu}>About me</Link>
+        <Link to="/projects" onClick={toggleMenu}>Projects</Link>
+        <Link to="/interview" onClick={toggleMenu}>Interview</Link>
       </nav>
     </header>
   );
